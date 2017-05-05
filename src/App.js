304741164848bd27c50975e4a6ff619eb8f0bc8e
Brian Ethier <davidbrianethier@gmail.com>
2017-05-05 15:11:15 -0600

    Getting rid of react logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 import Web3 from 'web3';
 var ens = require('./ensutils.js').ENS;
+
 var ETHEREUM_CLIENT = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/xRsYoyjxnWPw537pet38`))
 var instance = new ens(ETHEREUM_CLIENT)
 
@@ -36,18 +36,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>ENS</h2>
-        </div>
-        <div>
-          <form onSubmit={this.handleSubmit}>
-            <input type="name" value={this.state.value} onChange={this.handleChange} />
-            <input type="submit" value="Submit" />
-          </form>
-          <p>{this.state.ownerAddress}</p>
-          {/*<Button onPress={onButtonPress} title="button"/>*/}
-        </div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="name" value={this.state.value} onChange={this.handleChange} />
+          <input type="submit" value="Submit" />
+        </form>
+        <p>{this.state.ownerAddress}</p>
+        {/*<Button onPress={onButtonPress} title="button"/>*/}
       </div>
     );
   }
