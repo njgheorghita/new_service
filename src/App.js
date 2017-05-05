121b677381293fbd7d49dcc6ab3ed1abc54879f6
Brian Ethier <davidbrianethier@gmail.com>
2017-05-05 16:25:56 -0600

    Adding in express server

--- src/App.js
+++ src/App.js
@@ -16,7 +16,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {value: '',
-                  ownerAddress: ''};
+                  ownerAddress: '',
+                  //TODO: Take this attribute out. Just a hello world for hitting the server
+                  helloTarget: ''};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -31,16 +33,24 @@ class App extends Component {
     this.setState({ownerAddress: getContractOwner(this.state.value)})
     // this.props.getBookData(this.state.value);
     // this.setState({value: ''});
+
+    //This is just an example way to hit our express server running on port 3001.
+    //TODO: Take this out once we build fetching of rainbow tables etc
+    fetch('/hello')
+      .then(res => res.json())
+      .then(resp => this.setState({ helloTarget: resp.helloTarget }) );
   }
 
   render() {
     return (
       <div className="App">
         <form onSubmit={this.handleSubmit}>
+          <label>Enter address: </label>
           <input type="name" value={this.state.value} onChange={this.handleChange} />
           <input type="submit" value="Submit" />
         </form>
-        <p>{this.state.ownerAddress}</p>
+        <p>Owner Address: {this.state.ownerAddress}</p>
+        <p>Hello: {this.state.helloTarget ? this.state.helloTarget : "Noone yet"}</p>
         {/*<Button onPress={onButtonPress} title="button"/>*/}
       </div>
     );
